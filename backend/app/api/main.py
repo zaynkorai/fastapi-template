71311775e4af01ce12b6a53171d6f222d8c1f718
zaynkorai <zaynulabdin313@gmail.com>
2025-12-24 10:37:44 +0500

    full stack fastapi template

---
+++ backend/app/api/main.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter
+
+from app.api.routes import items, login, onboarding, private, teams, users, utils
+from app.core.config import settings
+
+api_router = APIRouter()
+api_router.include_router(login.router)
+api_router.include_router(users.router)
+api_router.include_router(utils.router)
+api_router.include_router(items.router)
+api_router.include_router(teams.router)
+api_router.include_router(onboarding.router)
+
+
+if settings.ENVIRONMENT == "local":
+    api_router.include_router(private.router)
